7acd52381a086416c2e5c9cbe811ee0715e80a4b
SysPoe <panddplayz@outlook.com>
2026-07-16 13:06:17 +1000

    update font

--- main.go
+++ main.go
@@ -276,7 +276,7 @@ func redundancyConfig(settings config.Settings) redundancy.Config {
 
 func newTheme() *material.Theme {
 	th := material.NewTheme()
-	th.Face = font.Typeface("Arial, Nirmala UI, Ubuntu, Times New Roman")
+	th.Face = font.Typeface("Aptos, Arial, Nirmala UI, Ubuntu, Times New Roman")
 
 	th.Palette.Bg = palette.Background
 	th.Palette.ContrastBg = palette.SurfaceRaised
